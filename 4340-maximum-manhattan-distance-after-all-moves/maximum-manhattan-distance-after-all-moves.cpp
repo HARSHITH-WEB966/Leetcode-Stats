e221175619eaa30957e618042428d7465fc101c9
HARSHITH-WEB966 <ballipalliharshithsaiteja2006@gmail.com>
2026-08-08 23:43:09 +0530

    Time: 12 ms (73.45%) | Memory: 22.3 MB (7.61%) - LeetSync

---
+++ 4340-maximum-manhattan-distance-after-all-moves/maximum-manhattan-distance-after-all-moves.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDistance(string moves) {
+     int u=0,d=0,l=0,r=0,w=0;
+    for(char c: moves){
+        if(c=='U') u++;
+        else if(c=='D') d++;
+        else if(c=='L') l++;
+        else if(c=='R') r++;
+        else w++;
+    }
+        return abs(l-r)+abs(u-d)+w;
+    }
+};
