f57de7b3db6c8a50942e44d035a9479a6478e162
HARSHITH-WEB966 <ballipalliharshithsaiteja2006@gmail.com>
2026-09-03 23:18:55 +0530

    Time: 14 ms (10.72%) | Memory: 47.4 MB (7.52%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,6 +1,6 @@
 class Solution {
     public String countAndSay(int n) {
-        if(n==1) return "1";
+        if(n==1) return "1"; 
 
         String prev=countAndSay(n-1);
         int len=prev.length(),c=1;
